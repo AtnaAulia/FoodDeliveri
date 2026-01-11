2f12c3dd587bcd93023a7f02f1f722aa0a10217e
AtnaAulia <ayly5080@gmail.com>
2026-01-11 23:12:05 +0700

    AKU UPDATE GIF JADI DELETE

--- app/Views/customers/index.php
+++ app/Views/customers/index.php
@@ -88,8 +88,8 @@
 
               <h4>Hapus Data Customer</h4>
               <div class="modal-gif">
+                <!-- PR TEST POP-UP -->
                 <img class="center" src="<?= base_url('assets/images/delete.gif'); ?>" alt="Warning">
-                
               </div>
 
               <p class="text-muted">Apakah Anda yakin ingin menghapus data ini?</p>
