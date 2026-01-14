e7ebbe233f9264bb4be5ef19ecef1050499016e0
RizkiBnjr <andriyanrizki8@gmail.com>
2026-01-14 22:18:33 +0700

    perbarui

--- app/Views/restaurants/edit.php
+++ app/Views/restaurants/edit.php
@@ -35,9 +35,9 @@
             <div class="mb-3">
                 <label class="form-label">Status</label>
                     <select class="form-control" name="status" class="form-select" value="<?= $restaurants['status'] ?>">
-                      <option value="Open" <?= $restaurants['status']=='Open'?'selected':'' ?>>beroperasi</option>
-                      <option value="Rest" <?= $restaurants['status']=='Rest'?'selected':'' ?>>tutup</option>
-                      <option value="Close" <?= $restaurants['status']=='Close'?'selected':'' ?>>istirahat </option>
+                      <option value="Open" <?= $restaurants['status']=='Open'?'selected':'' ?>>Open</option>
+                      <option value="Rest" <?= $restaurants['status']=='Rest'?'selected':'' ?>>Rest</option>
+                      <option value="Close" <?= $restaurants['status']=='Close'?'selected':'' ?>>Close</option>
                     </select>
             </div>
 
