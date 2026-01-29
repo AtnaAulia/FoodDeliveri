916437586a4229f1f8ba9fd92f2516f19f038d93
kylahys <kylahys12.com>
2026-01-29 09:36:22 +0700

    login

--- app/Views/auth/login.php
+++ app/Views/auth/login.php
@@ -25,41 +25,32 @@
                 <div class="alert alert-danger">
                     <?= session()->getFlashdata('error') ?>
                 </div>  
-            <?php endif; ?>
+    <?php endif; ?>
+
     <div class="container-scroller">
-      <div class="container-fluid page-body-wrapper full-page-wrapper">
-        <div class="row w-100">
-          <div class="content-wrapper full-page-wrapper d-flex align-items-center auth login-bg">
+      <div class="page-body-wrapper full-page-wrapper">
+        <div class="main-panel">
+          <div class="content-wrapper full-page-wrapper d-flex align-items-center auth">
             <div class="card col-lg-4 mx-auto">
               <div class="card-body px-5 py-5">
                 <h3 class="card-title text-start mb-3">Login</h3>
                 <form method="post" action="<?= base_url('login') ?>">
                   <?= csrf_field() ?>
+
                   <div class="form-group">
-                    <label>Username or email *</label>
+                    <label>Username</label>
                     <input type="text" class="form-control p_input" name="username" required>
                   </div>
+
                   <div class="form-group">
-                    <label>Password *</label>
+                    <label>Password</label>
                     <input type="password" class="form-control p_input" name="password" required>
                   </div>
-                  <div class="form-group d-flex align-items-center justify-content-between">
-                    <div class="form-check">
-                      <label class="form-check-label">
-                        <input type="checkbox" class="form-check-input"> Remember me </label>
-                    </div>
-                    <a href="#" class="forgot-pass">Forgot password</a>
-                  </div>
+    
                   <div class="text-center d-grid gap-2">
                     <button class="btn btn-primary btn-block enter-btn">Login</button>
                   </div>
-                  <div class="d-flex">
-                    <button class="btn btn-facebook me-2 col">
-                      <i class="mdi mdi-facebook"></i> Facebook </button>
-                    <button class="btn btn-google col">
-                      <i class="mdi mdi-google-plus"></i> Google plus </button>
-                  </div>
-                  <p class="sign-up">Don't have an Account?<a href="#"> Sign Up</a></p>
+                  
                 </form>
               </div>
             </div>
@@ -82,5 +73,5 @@
     <script src="../../assets/js/settings.js"></script>
     <script src="../../assets/js/todolist.js"></script>
     <!-- endinject -->
-  </body>
+  </body> 
 </html>
